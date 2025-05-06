bf51941f14366b3f8bc6f09b88646646731c7460
Bálint Gaganetz <balint.gaganetz@gmail.com>
2025-05-06 17:53:23 +0200

    controller elkészítése

---
+++ BACKEND/KEZDOCSAPATXI/Controllers/LineupController.cs
@@ -0,0 +1,22 @@
+﻿using KEZDOCSAPATXI.Models;
+using KEZDOCSAPATXI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KEZDOCSAPATXI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LineupController(IRatingGeneratorService service) : ControllerBase
+    {
+        private readonly IRatingGeneratorService _generatorService = service;
+        [HttpPost]
+        public ActionResult<Lineup> GenerateResult([FromBody] List<Player> players)
+        {
+            try
+            {
+                return Ok(_generatorService.GenerateAllLineups(players));
+            }
+            catch (Exception) { return BadRequest(); }
+        }
+    }
+}
