aeb491c35e319fd49f6965b3ee151909d64d6f07
Bálint Gaganetz <balint.gaganetz@gmail.com>
2025-05-06 09:21:17 +0200

    FormationExtension osztály elkészítése

---
+++ BACKEND/KEZDOCSAPATXI/Helpers/FormationExtension.cs
@@ -0,0 +1,18 @@
+﻿using KEZDOCSAPATXI.Models;
+
+namespace KEZDOCSAPATXI.Helpers
+{
+    public static class FormationExtension
+    {
+        public static Dictionary<string, int> ToPositionRequirements(this Formation f)
+        {
+            return new Dictionary<string, int>
+        {
+            { "GK", f.GK },
+            { "DF", f.DF },
+            { "MF", f.MF },
+            { "FW", f.FW }
+        };
+        }
+    }
+}
